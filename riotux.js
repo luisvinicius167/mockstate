2a96832a9cd389d3c147d6c150eb4184b2019e2a
Luis Vinicius <dev_luis@hotmail.com>
2016-02-16 20:18:02 -0200

    delete addNamespaces method

--- riotux.js
+++ riotux.js
@@ -10,56 +10,37 @@
 */
 
 ;(function( window ) {
-var Riotux = {
-  // the easy way to separate namespace to event
-  separate: ':',
-  centralDispatcher: new GlobalObserver(),
-  namespaces: [],
-  stores: {},
-  addStore: function (storeName, store) {
-    this.stores[storeName] = store;
-  },
-  addNamespace: function ( observer, method) {
-    this.namespaces.push(observer + this.separate + method);
-  },
-  getNamespaces: function ( ) {
-    return this.namespaces;
-  },
-  getStores: function ( ) {
-     return this.stores;
-  },
-  removeStores: function () {
-    this.stores = {};
-  },
-  getNamespace: function ( name ) {
-    var namespaces = [];
-    this.namespaces.forEach(function ( index ) {
-      var split = index.split(Riotux.separate)
-      if ( split[0] === name ) {
-        namespaces.push(split[1]);
-      }
-    });
-    return namespaces;
+  var Riotux = {
+    separate: ':',
+    centralDispatcher: new GlobalObserver(),
+    stores: {},
+    addStore: function (storeName, store) {
+      this.stores[storeName] = store;
+    },
+    getStores: function ( ) {
+      return this.stores;
+    },
+    removeStores: function () {
+      this.stores = {};
+    }
+  };
+
+  var _riot_methods = ['on','one','off','trigger'];
+  // provides riot observable api for Riotux
+  _riot_methods.forEach(function ( api ) {
+    Riotux[api] = function ( ) {
+      var args = [].slice.call(arguments);
+      var names = args[0].split(this.separate);
+      this.centralDispatcher[api].apply(null, args);
+    }
+  })
+
+  function GlobalObserver () {
+    riot.observable(this);
   }
-};
 
-var _riot_methods = ['on','one','off','trigger'];
-// provides riot observable api for Riotux
-_riot_methods.forEach(function ( api ) {
-  Riotux[api] = function ( ) {
-    var args = [].slice.call(arguments);
-    var names = args[0].split(this.separate);
-    Riotux.addNamespace(names[0], names[1]);
-    this.centralDispatcher[api].apply(null, args);
+  if ( !window.Riotux ) {
+    window.Riotux = Riotux;
   }
-})
-
-function GlobalObserver () {
-  riot.observable(this);
-}
-
-if ( !window.Riotux ) {
-  window.Riotux = Riotux;
-}
-if (typeof(module) !== 'undefined')  module.exports = Riotux;
+  if (typeof(module) !== 'undefined')  module.exports = Riotux;
 }(window));
