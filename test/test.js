58f83b9ad90e44bd8169abf91448a9f6a581fe04
luisvinicius167 <dev_luis@hotmail.com>
2016-03-27 16:45:36 -0300

    removing file

--- test/test.js
+++
@@ -1,7 +0,0 @@
-describe('carStore', function(){
-  describe('#personValue', function(){
-    it('should return the value pass with argument', function ( ) {
-      chai.assert.equal(1, riotux.trigger('carStore', 'start', 1));
-    });
-  });
-});
