5a0c7a5ac36672b0375ca71ea41f381efaf7b5a4
luisvinicius167 <dev_luis@hotmail.com>
2016-04-11 23:34:06 -0300

    add test to all stores that listening the event

--- test/stores/PersonStore.js
+++ test/stores/PersonStore.js
@@ -3,9 +3,11 @@ function PersonStore ( ) {
   this.on('setName', function ( name ) {
     window.personName = name;
   });
+  this.on('getName', function(){
+    console.log('personStore');
+    window.personGetNameEvent = true;
+  })
 };
 
 var personStore = new PersonStore();
 riotux.addStore('personStore', personStore);
-
-/** Testing */
