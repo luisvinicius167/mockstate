c194cab459ece72d297a599526da485edf9a17fc
Luis Vinicius <luis@startamus.com.br>
2016-10-14 21:17:11 -0300

    adding docs

--- src/riotux.js
+++ src/riotux.js
@@ -1,27 +1,43 @@
 'use strict';
 
+/**
+ * @name riotux
+ * @description The object that will manage all application state
+ */
 let riotux = {
   /**
    * @name _store
    * @description The private store
    */
   _store: {
+    /**
+     * @name state
+     * @description The Store application state
+     */
     state: {},
+    /**
+     * @name mutators
+     * @description The Functions that will change the Store state
+     */
     mutators: {},  
+    /**
+     * @name state
+     * @description The Components that was subscribed
+     */
     components: []
   },
   /**
    * @name store
-   * @description Public store
+   * @description Public Store
    */
   store: {
   	subscribe: (component, handler) => {
   		riotux._store.components.push({component, handler});
   	},
   	unsubscribe: (component) => {
-      riotux._store.components.forEach(el, index=>{
+      riotux._store.components.forEach(el, index => {
         if (el === component) {
-          Array.prototype.splice(this, index);
+          riotux._store.components.splice(index, 1);
         }
       });
     },
@@ -30,7 +46,7 @@ let riotux = {
      * @description Dispatch an action to change
      * the store state
      * @param { string } action The action name
-     * @param { string } args Arguments sended to the action 
+     * @param { any } args Arguments sended to the action 
      */
     dispatch: (action, ...args) => {
       let state;
@@ -61,17 +77,22 @@ let riotux = {
     /**
      * @name setState
      * @description Sets the application data state
+     * @param {object} data Simple Object that contain the State
      */
     setState: (data) => {
       Object.assign(riotux._store.state, data);
     },
+    /**
+     * @name get
+     * @param {string} stateName The Store state name
+     */
     get: (stateName) => {
       return riotux._store.state[stateName];
     },
     /**
      * @name setMutators
      * @description Sets the application data mutators
-     * that will change the _store state
+     * that will change the Store state
      */
     setMutators: (data) => {
       Object.assign(riotux._store.mutators, data);
