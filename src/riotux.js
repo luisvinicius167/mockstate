dd586083f70ce9beb69223e961efbdca95569662
Luis Vinicius <luis@startamus.com.br>
2016-10-18 22:41:13 -0200

    middleware support.

--- src/riotux.js
+++ src/riotux.js
@@ -1,4 +1,4 @@
-(function(root, factory){
+;(function(root, factory){
 	if (typeof define === "function" && define.amd) {
         define([], factory());
     } else if (typeof exports === "object") {
