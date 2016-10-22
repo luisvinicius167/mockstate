d322fc4b40f3dc9f66621011f36c6dd661dc00f8
Luis Vinicius <luis@startamus.com.br>
2016-10-22 00:11:35 -0200

    add tests

--- tests/setState.js
+++ tests/setState.js
@@ -1,13 +1,36 @@
 expect = chai.expect;
 
-Mockstate.setState({
-  count: 1
-});
-
 describe('StoreState', function () {
-  describe('#count', function () {
+
+  describe('#getState(count)', function () {
     it.only('should be 1', function () {
       expect(Mockstate.getState('count')).to.equal(1);
+    });
+  });
+
+  describe('#IncrementCount', function () {
+    it.only('The state count should be 2', function () {
+      Mockstate.dispatch('increment').then(function (data) {
+        expect(data.value).to.equal(2);
+      })
+    });
+  });
+
+  describe('#IncrementCountInParalel', function () {
+
+    it.only('Verify the result of the action', function () {
+      Mockstate.dispatch('increment')
+        .then(function (data) {
+          expect(data.value).to.equal(3);
+        })
+      Mockstate.dispatch('increment')
+        .then(function (data) {
+          expect(data.value).to.equal(4);
+        })
+    });
+
+    it.only('Verify the value of count', function () {
+      expect(Mockstate.getState('count')).to.equal(4);
     });
   });
 });
